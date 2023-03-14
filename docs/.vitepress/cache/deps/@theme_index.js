f250a20cf453e9be80f1d4b382184e0aa700451c
wl1532502541 <1532502541@qq.com>
2023-03-14 22:41:57 +0800

    feat: 支持 PWA

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -1,24 +1,24 @@
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
-import "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import NotFound from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
-import { default as default2 } from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default3 } from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default4 } from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default5 } from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default6 } from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default7 } from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default8 } from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default9 } from "/Users/wanglei12/Desktop/project/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import NotFound from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/NotFound.vue";
+import { default as default2 } from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "E:/projects/FE-Knowledge2/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 var theme = {
   Layout,
   NotFound,
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,8 @@
 import * as fs from "fs"
 import * as path from "path"
 import * as process from "process"
+import { defineConfig } from 'vitepress'
+import { withPwa } from '@vite-pwa/vitepress'
 
 const ignoreList = [".vitepress", "README.md", "public", "index.md", ".DS_Store"];
 
@@ -26,72 +28,120 @@ function buildChildren(path, parentName = "") {
 const workPath = path.join(process.cwd() + "/docs");
 const sidebar = buildChildren(workPath);
 
-export default {
-  title: '前端知识整理',
-  head: [
-    ['meta', { name: 'referrer', content: 'never' }],//会出现在html的head里，用来绕过语雀的图片防盗链
-    ['link', { href: './logo/klee.ico', rel: 'SHORTCUT ICON' }]
-  ],
-  base: "/FE-Knowledge2/",
-  lastUpdated: true,
-  themeConfig: {
-    docsDir: 'docs',
-    logo: '/logo/book.png',
-    nav: [
-      { text: '首页', link: '/' }
-    ],
-    sidebar,
-    footer: {
-      message: 'MIT Licensed',
-      copyright: 'Copyright © 2021-present Leinaldo'
-    },
-    algolia: {
-      appId: 'F84THJ1DR1',
-      apiKey: '7811d185eeb9a69f4972c25e4169ca58',
-      indexName: 'fe-knowledge2',
-      locales: {
-        zh: {
-          placeholder: '搜索文档',
-          translations: {
-            button: {
-              buttonText: '搜索文档',
-              buttonAriaLabel: '搜索文档'
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: '清除查询条件',
-                resetButtonAriaLabel: '清除查询条件',
-                cancelButtonText: '取消',
-                cancelButtonAriaLabel: '取消'
-              },
-              startScreen: {
-                recentSearchesTitle: '搜索历史',
-                noRecentSearchesText: '没有搜索历史',
-                saveRecentSearchButtonTitle: '保存至搜索历史',
-                removeRecentSearchButtonTitle: '从搜索历史中移除',
-                favoriteSearchesTitle: '收藏',
-                removeFavoriteSearchButtonTitle: '从收藏中移除'
-              },
-              errorScreen: {
-                titleText: '无法获取结果',
-                helpText: '你可能需要检查你的网络连接'
-              },
-              footer: {
-                selectText: '选择',
-                navigateText: '切换',
-                closeText: '关闭',
-                searchByText: '搜索提供者'
-              },
-              noResultsScreen: {
-                noResultsText: '无法找到相关结果',
-                suggestedQueryText: '你可以尝试查询',
-                reportMissingResultsText: '你认为该查询应该有结果？',
-                reportMissingResultsLinkText: '点击反馈'
+export default withPwa(
+  defineConfig(
+    {
+      vite: {
+        logLevel: 'info',
+        define: {
+          __DATE__: `'${new Date().toISOString()}'`,
+        },
+      },
+      title: '前端知识整理',
+      head: [
+        ['meta', { name: 'referrer', content: 'never' }],//会出现在html的head里，用来绕过语雀的图片防盗链
+        ['link', { href: './logo/klee.ico', rel: 'SHORTCUT ICON' }]
+      ],
+      base: "/FE-Knowledge2/",
+      lastUpdated: true,
+      themeConfig: {
+        docsDir: 'docs',
+        logo: '/logo/book.png',
+        nav: [
+          { text: '首页', link: '/' }
+        ],
+        sidebar,
+        footer: {
+          message: 'MIT Licensed',
+          copyright: 'Copyright © 2021-present Leinaldo'
+        },
+        algolia: {
+          appId: 'F84THJ1DR1',
+          apiKey: '7811d185eeb9a69f4972c25e4169ca58',
+          indexName: 'fe-knowledge2',
+          locales: {
+            zh: {
+              placeholder: '搜索文档',
+              translations: {
+                button: {
+                  buttonText: '搜索文档',
+                  buttonAriaLabel: '搜索文档'
+                },
+                modal: {
+                  searchBox: {
+                    resetButtonTitle: '清除查询条件',
+                    resetButtonAriaLabel: '清除查询条件',
+                    cancelButtonText: '取消',
+                    cancelButtonAriaLabel: '取消'
+                  },
+                  startScreen: {
+                    recentSearchesTitle: '搜索历史',
+                    noRecentSearchesText: '没有搜索历史',
+                    saveRecentSearchButtonTitle: '保存至搜索历史',
+                    removeRecentSearchButtonTitle: '从搜索历史中移除',
+                    favoriteSearchesTitle: '收藏',
+                    removeFavoriteSearchButtonTitle: '从收藏中移除'
+                  },
+                  errorScreen: {
+                    titleText: '无法获取结果',
+                    helpText: '你可能需要检查你的网络连接'
+                  },
+                  footer: {
+                    selectText: '选择',
+                    navigateText: '切换',
+                    closeText: '关闭',
+                    searchByText: '搜索提供者'
+                  },
+                  noResultsScreen: {
+                    noResultsText: '无法找到相关结果',
+                    suggestedQueryText: '你可以尝试查询',
+                    reportMissingResultsText: '你认为该查询应该有结果？',
+                    reportMissingResultsLinkText: '点击反馈'
+                  }
+                }
               }
             }
           }
         }
+      },
+      pwa: {
+        mode: 'development',
+        base: '/',
+        scope: '/',
+        registerType: 'autoUpdate',
+        // injectRegister: 'inline',
+        // includeAssets: ['favicon.svg'],
+        manifest: {
+          name: 'FE-Knowledge',
+          short_name: 'FEKnowledge',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'colagirl-192x192',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'colagirl-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'colagirl-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+        },
+        workbox: {
+          globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'],
+        },
+        devOptions: {
+          enabled: true,
+          navigateFallback: '/',
+        },
       }
     }
-  }
-}
+  )
+)
