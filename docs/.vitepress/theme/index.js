4ac525e22921fffbe37de32a1efee76fbfc91a3f
wl1532502541 <1532502541@qq.com>
2024-09-22 05:13:22 +0800

    feat: 支持评论

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,5 @@
 import MyLayout from './MyLayout.vue'
 import './custom.css'
-import Giscus from '@giscus/vue';
 import { AntDesignContainer, ElementPlusContainer, NaiveUIContainer } from '@vitepress-demo-preview/component'
 import '@vitepress-demo-preview/component/dist/style.css'
 
@@ -8,6 +7,5 @@ export default {
   Layout: MyLayout,
   enhanceApp: async ({ app }) => {
     app.component('demo-preview', NaiveUIContainer)
-    app.component('Giscus', Giscus)
   },
 }
