a024198ca27243a18abed7e1285b64d767c67b4c
wl1532502541 <1532502541@qq.com>
2024-10-04 03:43:34 +0800

    update

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -1,22 +1,16 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 import CommentGiscus from './components/CommentGiscus.vue'
-import naive from 'naive-ui'
-import { useData } from 'vitepress';
 
-const { NConfigProvider, darkTheme } = naive
 
-const { isDark } = useData()
 
 const { Layout } = DefaultTheme
 </script>
 
 <template>
-  <NConfigProvider :theme="isDark ? darkTheme : null">
-    <Layout>
-      <template #doc-after>
-        <CommentGiscus />
-      </template>
-    </Layout>
-  </NConfigProvider>
+  <Layout>
+    <template #doc-after>
+      <CommentGiscus />
+    </template>
+  </Layout>
 </template>
